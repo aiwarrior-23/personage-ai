2751c2d580e213d6ebead81f799cabe1d2d92c60
SandipBehera <sandip.sudip36@gmail.com>
2024-01-30 19:04:23 +0530

    frontend code pushed

---
+++ frontend/src/Config/ThemeConfig.jsx
@@ -0,0 +1,16 @@
+export class ConfigDB {
+  static data = {
+    settings: {
+      layout_type: "ltr",
+      sidebar: { type: "horizontal-wrapper", iconType: "stroke-svg" },
+      sidebar_setting: "default-sidebar",
+    },
+    color: {
+      primary_color: "#7366ff",
+      secondary_color: "#ae4761",
+      mix_background_layout: "light-only",
+    },
+    router_animation: "fadeIn",
+  };
+}
+export default ConfigDB;
---
+++ frontend/src/Layout/Sidebar/SidebarIcon.jsx
@@ -0,0 +1,17 @@
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import cubaimg from "../../assets/images/logo/logo-icon.png";
+import CustomizerContext from "../../_helper/Customizer";
+
+const SidebarIcon = () => {
+  const { layoutURL } = useContext(CustomizerContext);
+  return (
+    <div className="logo-icon-wrapper">
+      <Link to={`/dashboard/default/${layoutURL}`}>
+        <img className="img-fluid" src={cubaimg} alt="" />
+      </Link>
+    </div>
+  );
+};
+
+export default SidebarIcon;
