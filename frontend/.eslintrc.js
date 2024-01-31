a6dc0ff97f28989f7ab8c1b4ccc254b33d7f9bc0
SandipBehera <sandip.sudip36@gmail.com>
2024-01-31 20:23:29 +0530

    added es

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  // other ESLint configurations...
+  parserOptions: {
+    ecmaVersion: 2021, // or a version that supports ECMAScript modules
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  // other configurations...
+};
