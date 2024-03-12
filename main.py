baba46dd113a6c59cf59ef81b278a50c11c4cefe
aiwarrior-23 <himanshuit3036@gmail.com>
2024-03-12 17:50:20 +0530

    new changes added

--- main.py
+++ main.py
@@ -120,29 +120,45 @@ def validate_password(username, password):
 
     return response, status_code
 
-def check_user_credentials(username, password):
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT password, username FROM company_passwords WHERE username = %s", (username,))
-    user_record = cur.fetchone()
-    cur.close()
+from logger import get_logger
 
-    if user_record and user_record[0] == password:
-        return user_record[1], True
-    else:
+def check_user_credentials(cursor, username, password):
+    logger = get_logger("my_logger")
+    try:
+        cur = cursor
+        cur.execute("SELECT company_passwords.password, company_passwords.username, company_table.* FROM company_passwords LEFT JOIN company_table ON company_table.username = company_passwords.username WHERE company_passwords.username = %s AND company_passwords.password = %s", (username, password))
+        columns = [col[0] for col in cur.description]
+        user_record = cur.fetchone()
+        cur.close()
+        if user_record and user_record[0] == password:  # Access password using index
+            user_dict = dict(zip(columns, user_record))
+            return user_dict, True
+        else:
+            return None, False
+
+    except Exception as e:
+        # Handle the exception, you might want to log it or return a specific error
+        print(f"Error: {e}")
+        logger.error(str(e))
         return None, False
 
-@app.route('/validate_credentials', methods=['POST'])
-def validate_credentials():
-    data = request.json
-    username = data.get('username')
-    password = data.get('password')
+def validate_password(cursor,username, password):
+    cur = cursor
 
-    user_type, is_valid = check_user_credentials(username, password)
+    # Insert username and password into company_passwords table
+    try:
+        cur.execute("INSERT INTO company_passwords(username, password) VALUES (%s, %s)", (username, password))
+        mysql.connection.commit()
+        response = {"message": f"User {username} added to company_passwords table."}
+        status_code = 200
+    except Exception as e:
+        mysql.connection.rollback()
+        response = {"message": str(e)}
+        status_code = 400
+    finally:
+        cur.close()
 
-    if is_valid:
-        return jsonify({"user_type": user_type, "valid": True}), 200
-    else:
-        return jsonify({"message": "Invalid credentials", "valid": False}), 401
+    return response, status_code
 
 @app.route('/login', methods=['POST'])
 def login():
